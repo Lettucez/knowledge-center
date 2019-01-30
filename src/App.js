6435a84b838946a985d642095d5377fdbc8c1141
Christopher Leo <cleo@wvuf.org>
2019-01-30 14:39:27 -0500

    add searchbar component

--- src/App.js
+++ src/App.js
@@ -7,18 +7,39 @@ import TitleText from './components/TitleText/TitleText';
 import SearchBar from './components/SearchBar/SearchBar';
 import Breadcrumbs from './components/Breadcrumbs/Breadcrumbs';
 import Spinner from './components/Spinner/Spinner';
+import Fuse from 'fuse.js';
 
 import allKCItems from './testState';
+
+
+const options = {
+    shouldSort: true,
+    threshold: 0.4,
+    location: 0,
+    distance: 100,
+    maxPatternLength: 32,
+    minMatchCharLength: 1,
+    keys: [
+        "title",
+        "searchData"
+    ]
+};
 
 const App = (props) => {
 
     //set up app state using hooks
     //all kc items
     const [initialKCItems, setInitialKCItems] = useState([]);
-    //kc items by category
+    //items that are shown, filtered by either top level categories, clicked category, or search term
     const [filteredKCItems, setFilteredKCItems] = useState([]);
+    //items that are top level categories only
+    const [topLevelKCItems, setTopLevelKCItems] = useState([]);
     //breadcrumb name to show
     const [breadcrumbCategoryName, setBreadcrumbCategoryName] = useState(null);
+    //hold search term
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const fuse = new Fuse(initialKCItems, options);
 
     //populate initial Knowledge Center items
     //TODO change function to use axios and get items from drupal
@@ -30,6 +51,7 @@ const App = (props) => {
                 return !item.parentReference.length;
             });
             setInitialKCItems(allKCItems);
+            setTopLevelKCItems(filteredItems);
             setFilteredKCItems(filteredItems);
         }, 3000);
 
@@ -40,27 +62,31 @@ const App = (props) => {
         const category = filteredKCItems.find((item) => {
             return item.id === kcID;
         });
-        setBreadcrumbCategoryName(category.title);
 
-        //find kc items with parent of clicked category
-        const filteredItems = [...initialKCItems].filter((item) => {
+        const filteredItems = initialKCItems.filter((item) => {
             return item.parentReference.some((parentRef) => {
                 return parentRef.id === kcID;
             });
         });
         setFilteredKCItems(filteredItems);
+        setBreadcrumbCategoryName(category.title);
+        setSearchTerm('');
     };
 
+    //set filtered items to initial items with no parentRef field
     const handleHomeBreadcrumbClick = () => {
-        const filteredItems = initialKCItems.filter((item) => {
-            return !item.parentReference.length;
-        });
-        setFilteredKCItems(filteredItems);
+        setFilteredKCItems(topLevelKCItems);
         setBreadcrumbCategoryName(null);
+        setSearchTerm('');
     };
 
-    const handleSearchText = (searchItems) => {
-        setFilteredKCItems(searchItems);
+    //handle search
+    const handleSearchTerm = (event) => {
+        setSearchTerm(event.target.value);
+        setFilteredKCItems(fuse.search(event.target.value));
+        if (event.target.value === '') {
+            setFilteredKCItems(topLevelKCItems);
+        }
     };
 
 
@@ -69,7 +95,7 @@ const App = (props) => {
             <div className="section">
                 <div className="container">
                     <TitleText/>
-                    <SearchBar initialKC={initialKCItems} filteredItems={filteredKCItems} onSearch={handleSearchText} />
+                    <SearchBar onSearch={handleSearchTerm} searchTerm={searchTerm}/>
                     <hr style={{backgroundColor: "#dbdbdb"}}/>
                     <Breadcrumbs categoryName={breadcrumbCategoryName} homeClick={handleHomeBreadcrumbClick}/>
                     {initialKCItems.length ?
